a19d00bb81762e0db36ed04bf7a388eb197af5eb
aht33 <alexhtang03@gmail.com>
2023-09-26 17:48:20 -0500

    fixed user auth and css

--- firebase-react-auth/src/App.js
+++ firebase-react-auth/src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import Home from './components/Home';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
- 
+import React from "react";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
- 
   return (
     <Router>
       <div>
-        <section>                              
-            <Routes>                                                                        
-               <Route path="/signup" element={<Signup/>}/>
-               <Route path="/login" element={<Login/>}/>
-               <Route path="/" element={<Home/>}/>
-            </Routes>                    
+        <section>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
         </section>
       </div>
     </Router>
   );
 }
- 
+
 export default App;
